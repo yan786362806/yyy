18acf442a4fb22c30eca46cce4dc3f124e4a91d9
yan786362806 <786362806@qq.com>
2015-11-11 19:10:33 +0800

    你好
    终于能上传了

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  yyy
+//
+//  Created by 20141105044y on 15/11/11.
+//  Copyright © 2015年 20141105044y. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
